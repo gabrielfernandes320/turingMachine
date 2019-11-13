7949c1b9ecae154271d8747a7998b3d7a1bb0344
Laboratorio XXIA - 001 <lab001a@lab.unesc.net>
2019-11-13 21:53:17 -0200

    first commit

---
+++ turingnator/turingnator/MachineView.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace turingnator
+{
+    partial class MachineView
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiReset = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvStates = new System.Windows.Forms.DataGridView();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStates)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiStart,
+            this.tsmiReset});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(333, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "msMainMenu";
+            // 
+            // tsmiStart
+            // 
+            this.tsmiStart.Name = "tsmiStart";
+            this.tsmiStart.Size = new System.Drawing.Size(51, 20);
+            this.tsmiStart.Text = "Iniciar";
+            // 
+            // tsmiReset
+            // 
+            this.tsmiReset.Name = "tsmiReset";
+            this.tsmiReset.Size = new System.Drawing.Size(57, 20);
+            this.tsmiReset.Text = "Resetar";
+            // 
+            // dgvStates
+            // 
+            this.dgvStates.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.dgvStates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStates.Location = new System.Drawing.Point(12, 38);
+            this.dgvStates.Name = "dgvStates";
+            this.dgvStates.Size = new System.Drawing.Size(309, 218);
+            this.dgvStates.TabIndex = 1;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 279);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(308, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(333, 311);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dgvStates);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStates)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiStart;
+        private System.Windows.Forms.ToolStripMenuItem tsmiReset;
+        private System.Windows.Forms.DataGridView dgvStates;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ turingnator/turingnator/State.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace turingnator
+{
+    class State
+    {
+
+        private int id { get; set; }
+
+        MachineView Machine = new MachineView();
+
+        IDictionary<char, string> actions = new Dictionary<char, string>();
+
+
+        public State(int id, IDictionary<char, string> actions, MachineView Machine)
+        {
+
+            this.Machine = Machine;
+            this.id = id;
+            this.actions = actions;           
+
+        }
+
+        public string read(string strip)
+        {
+
+            strip = write(strip);
+
+            
+
+            return strip;
+
+        }
+
+        private void move()
+        {
+
+
+
+        }
+
+        private string write(string strip)
+        {
+
+            char towrite = actions[strip[Machine.position]][1];
+
+            StringBuilder head = new StringBuilder(strip);
+            head.Remove(Machine.position, 1);
+            head.Insert(Machine.position, towrite);
+            return strip;
+
+        }
+
+    }
+}
